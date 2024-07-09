b6f9057f4a7c1802fca1ea6a61fd3a561b34122c
thatoneade <153532914+thatoneade@users.noreply.github.com>
2024-07-09 21:39:53 +0100

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector("#nav-bar");
+const hamburger= document.querySelector("#hamburger");
+const closeIcon= document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
